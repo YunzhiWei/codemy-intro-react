410c28d168d4379542607a77509399b9a5453137
Chris Wei <yunzhi.wei@live.com>
2017-06-06 16:00:54 +0800

    nested components and props

--- app/components/Layout.js
+++ app/components/Layout.js
@@ -1,12 +1,34 @@
 import React from "react";
 
+const data = [
+  {
+    name: 'Chris Wei',
+    email: 'yunzhi.wei@live.com'
+  },
+  {
+    name: 'Clare Huang',
+    email: 'wulang@hotmail.com'
+  },
+  {
+    name: 'Jolin Wei',
+    email: 'jollin@qq.com'
+  },
+];
+
+const Contact = (props) =>
+  <div className='pure-u-1-3'>
+    <h3>{props.name}</h3>
+    <p>{props.email}</p>
+  </div>
+
+
 class Layout extends React.Component {
   render() {
     return(
       <div id='Layout' className='pure-g'>
-        <div className="pure-u-1-3">1st. 1-3</div>
-        <div className="pure-u-1-3">2nd. 2-3</div>
-        <div className="pure-u-1-3">3rd. 3-3</div>
+        {data.map((info) =>
+          <Contact name={info.name} email={info.email} />
+        )}
       </div>
     )
   }
