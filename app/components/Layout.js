9dfe6ee9ccd4e61aa7bff29d073c9b156007f826
Chris Wei <yunzhi.wei@live.com>
2017-06-06 16:15:22 +0800

    Change the name use 'key' and 'id' to solve the warning

--- app/components/Layout.js
+++ app/components/Layout.js
@@ -1,14 +1,14 @@
 import React from "react";
 
-import data from './data';
+import ContactInfo from './data';
 import Contact from './Contact';
 
 class Layout extends React.Component {
   render() {
     return(
       <div id='Layout' className='pure-g'>
-        {data.map((info) =>
-          <Contact {...info} />
+        {ContactInfo.map((info) =>
+          <Contact key={info.id} {...info} />
         )}
       </div>
     )
--- app/components/data.js
+++ app/components/data.js
@@ -1,16 +1,19 @@
-const data = [
+const ContactInfo = [
   {
+    id: 1,
     name: 'Chris Wei',
     email: 'yunzhi.wei@live.com'
   },
   {
+    id: 2,
     name: 'Clare Huang',
     email: 'wulang@hotmail.com'
   },
   {
+    id: 3,
     name: 'Jolin Wei',
     email: 'jollin@qq.com'
   },
 ];
 
-export default data;
+export default ContactInfo;
