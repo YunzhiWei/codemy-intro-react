a19dc184ea7fa867c0317b90f5d6eb0eaec1ac97
Chris Wei <yunzhi.wei@live.com>
2017-06-06 16:26:41 +0800

    event and componenet methods

--- app/components/Layout.js
+++ app/components/Layout.js
@@ -4,12 +4,26 @@ import ContactInfo from './data';
 import Contact from './Contact';
 
 class Layout extends React.Component {
+  addContact = (e) => {
+    e.preventDefault();
+    console.log("button click");
+  };
+
   render() {
     return(
-      <div id='Layout' className='pure-g'>
-        {ContactInfo.map((info) =>
-          <Contact key={info.id} {...info} />
-        )}
+      <div id='Layout'>
+        <div>
+          <a  href="#"
+              className='pure-button'
+              onClick={this.addContact}>
+              Add Contact
+          </a>
+          <div className='pure-g'>
+            {ContactInfo.map((info) =>
+              <Contact key={info.id} {...info} />
+            )}
+          </div>
+        </div>
       </div>
     )
   }
