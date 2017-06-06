58af1575b6dbbaee7843375564f2e7c6f2864b95
Chris Wei <yunzhi.wei@live.com>
2017-06-06 14:58:58 +0800

    use purecss grid system

--- app/components/Layout.js
+++ app/components/Layout.js
@@ -3,8 +3,10 @@ import React from "react";
 class Layout extends React.Component {
   render() {
     return(
-      <div id='Layout'>
-        Hello from react
+      <div id='Layout' className='pure-g'>
+        <div className="pure-u-1-3">1st. 1-3</div>
+        <div className="pure-u-1-3">2nd. 2-3</div>
+        <div className="pure-u-1-3">3rd. 3-3</div>
       </div>
     )
   }
