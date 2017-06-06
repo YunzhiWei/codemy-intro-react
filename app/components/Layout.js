92a5a9de3efb374c6c9b6d20c5d68aca51cbc6e6
Chris Wei <yunzhi.wei@live.com>
2017-06-06 16:53:30 +0800

    add form use css file for style remove Add Contact button

--- app/components/Layout.js
+++ app/components/Layout.js
@@ -3,6 +3,8 @@ import React from "react";
 import ContactInfo from './data';
 import Contact from './Contact';
 
+import './Layout.css'
+
 class Layout extends React.Component {
   componentWillMount() {
     this.setState({
@@ -29,12 +31,19 @@ class Layout extends React.Component {
   render() {
     return(
       <div id='Layout'>
+        <div className='pure-g'>
+          <div className='pure-u-12-24'>
+            <form className="pure-form">
+              <fieldset>
+                <legend>New contact</legend>
+                <input type="email" placeholder="Email@domain.com" />
+                <input type="Text" placeholder="Name" />
+                <button type="submit" className="pure-button pure-button-primary">Sign in</button>
+              </fieldset>
+            </form>
+          </div>
+        </div>
         <div>
-          <a  href="#"
-              className='pure-button'
-              onClick={this.addContact}>
-              Add Contact
-          </a>
           <div className='pure-g'>
             {this.state.contacts.map((info) =>
               <Contact key={info.id} {...info} />
