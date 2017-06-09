36a415b4994f243c6fc12630da961f97e07cb255
Chris Wei <yunzhi.wei@live.com>
2017-06-09 21:40:16 +0800

    correct the display fields

--- app/components/Show.js
+++ app/components/Show.js
@@ -14,7 +14,7 @@ class Show extends React.Component {
     return(
       <div id='Show'>
         <h1>{this.state.contact.id}</h1>
-        <h2>{this.state.contact.name}</h2>
+        <h2>{`${this.state.contact.first_name} ${this.state.contact.last_name}`}</h2>
         <h3>{this.state.contact.email}</h3>
       </div>
     )
