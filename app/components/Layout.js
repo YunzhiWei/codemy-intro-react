d977f2d57385892377e60afdf43d1eb263dfded2
Chris Wei <yunzhi.wei@live.com>
2017-06-10 15:44:30 +0800

    add navbar

--- app/components/Layout.js
+++ app/components/Layout.js
@@ -1,8 +1,22 @@
 import React from 'react';
+import styles from './Layout.sass';
 
 const Layout = (props) =>
-  <div id="Layout">
+  <div id="Layout" className={styles.layout}>
+    <div className={`home-menu pure-menu pure-menu-horizontal pure-menu-fixed ${styles.mainNav}`}>
+      <a href='#' className={`pure-menu-heading ${styles.heading}`}>Invoiced</a>
+      <ul className='pure-menu-list'>
+        <li className='pure-menu-item'>
+          <a href='#' className={`pure-menu-link ${styles.links}`}>Sign In</a>
+        </li>
+      </ul>
+    </div>
     {props.children}
   </div>
 
 export default Layout;
+
+/*
+import classNames from 'classnames';
+classNames('home-menu', 'pure-menu', 'pure-menu-horizontal', 'pure-menu-fixed', styles.mainNav)
+*/
