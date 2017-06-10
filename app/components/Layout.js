927ccb0a8c7a7ae6928765d018e72e43a39e763d
Chris Wei <yunzhi.wei@live.com>
2017-06-10 16:09:43 +0800

    use Link to make it possible to back to home page and jump to log in page

--- app/components/Layout.js
+++ app/components/Layout.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import styles from './Layout.sass';
 
+import { Link } from 'react-router';
+
 const Layout = (props) =>
   <div id="Layout" className={styles.layout}>
     <div className={`home-menu pure-menu pure-menu-horizontal pure-menu-fixed ${styles.mainNav}`}>
-      <a href='#' className={`pure-menu-heading ${styles.heading}`}>Invoiced</a>
+      <Link to='/' className={`pure-menu-heading ${styles.heading}`}>Invoiced</Link>
       <ul className='pure-menu-list'>
         <li className='pure-menu-item'>
-          <a href='#' className={`pure-menu-link ${styles.links}`}>Sign In</a>
+          <Link to='/users/sign_in' className={`pure-menu-link ${styles.links}`}>Sign In</Link>
         </li>
       </ul>
     </div>
