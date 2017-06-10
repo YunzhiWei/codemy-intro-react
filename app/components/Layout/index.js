6a544802738c71aa81260cc9224cbbf5aa9de6bc
Chris Wei <yunzhi.wei@live.com>
2017-06-10 16:31:03 +0800

    upgrade the Layout from const to class be sure to use this.props.children instead of props.children

--- app/components/Layout/index.js
+++ app/components/Layout/index.js
@@ -3,18 +3,23 @@ import styles from './index.sass';
 
 import { Link } from 'react-router';
 
-const Layout = (props) =>
-  <div id="Layout" className={styles.layout}>
-    <div className={`home-menu pure-menu pure-menu-horizontal pure-menu-fixed ${styles.mainNav}`}>
-      <Link to='/' className={`pure-menu-heading ${styles.heading}`}>Invoiced</Link>
-      <ul className='pure-menu-list'>
-        <li className='pure-menu-item'>
-          <Link to='/users/sign_in' className={`pure-menu-link ${styles.links}`}>Sign In</Link>
-        </li>
-      </ul>
-    </div>
-    {props.children}
-  </div>
+class Layout extends React.Component {
+  render() {
+    return(
+      <div id="Layout" className={styles.layout}>
+        <div className={`home-menu pure-menu pure-menu-horizontal pure-menu-fixed ${styles.mainNav}`}>
+          <Link to='/' className={`pure-menu-heading ${styles.heading}`}>Invoiced</Link>
+          <ul className='pure-menu-list'>
+            <li className='pure-menu-item'>
+              <Link to='/users/sign_in' className={`pure-menu-link ${styles.links}`}>Sign In</Link>
+            </li>
+          </ul>
+        </div>
+        {this.props.children}
+      </div>
+    )
+  }
+}
 
 export default Layout;
 
