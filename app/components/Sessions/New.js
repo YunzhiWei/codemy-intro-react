4355072127c3b351ab4a62533c3b61f14ea94f0e
Chris Wei <yunzhi.wei@live.com>
2017-06-10 13:51:38 +0800

    Prepare for the sign in form

--- app/components/Sessions/New.js
+++ app/components/Sessions/New.js
@@ -1,11 +1,16 @@
 import React from 'react';
+import styles from './New.sass';
 
 class New extends React.Component {
   render () {
     return (
-      <form>
-        Sign in form
-      </form>
+      <div className={`${styles.signInForm}`}>
+        <form className='pure-form pure-form-stacked'>
+          <label>Email</label><input type='email' placeholder='email' className='pure-input-1' />
+          <label>Password</label><input type='password' placeholder='password' className='pure-input-1' />
+          <button className={`pure-button pure-input-1 ${styles.signInButton}`}>Sign In</button>
+        </form>
+      </div>
     )
   }
 }
