125506e1e28623fe0ed703dcf74da53f6e715a56
Chris Wei <yunzhi.wei@live.com>
2017-06-13 19:36:25 +0800

    prepare builder

--- webpack.config.js
+++ webpack.config.js
@@ -27,6 +27,25 @@ module.exports = {
         ]
       },
       {
+        test: /\.(jpe?g|png|gif|svg)$/i,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              hash: 'sha512',
+              digest: 'hex',
+              name: '[hash].[ext]'
+            }
+          },
+          {
+            loader: 'image-webpack-loader',
+            options: {
+              bypassOnDebug: true
+            }
+          }
+        ]
+      },
+      {
         test: /\.(scss|sass)$/,
         use: [
           {
