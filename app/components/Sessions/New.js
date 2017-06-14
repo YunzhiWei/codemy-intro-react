bb3daec992d7a881af77cb06e90de7742964df69
Chris Wei <yunzhi.wei@live.com>
2017-06-14 13:55:54 +0800

    UI style

--- app/components/Sessions/New.js
+++ app/components/Sessions/New.js
@@ -17,12 +17,14 @@ class New extends React.Component {
 
   render () {
     return (
-      <div className={`${styles.signInForm}`}>
-        <form className='pure-form pure-form-stacked' onSubmit={this.onSubmitForm}>
-          <label>Email</label><input type='email' ref={node => { this.email = node; }} placeholder='email' className='pure-input-1' />
-          <label>Password</label><input type='password' ref={node => { this.password = node; }} placeholder='password' className='pure-input-1' />
-          <button className={`pure-button pure-input-1 ${styles.signInButton}`}>Sign In</button>
-        </form>
+      <div className={`${styles.signInWrapper}`}>
+        <div>
+          <form className='pure-form pure-form-stacked' onSubmit={this.onSubmitForm}>
+            <label>Email</label><input type='email' ref={node => { this.email = node; }} placeholder='email' className='pure-input-1' />
+            <label>Password</label><input type='password' ref={node => { this.password = node; }} placeholder='password' className='pure-input-1' />
+            <button className={`pure-button pure-input-1 ${styles.signInButton}`}>Sign In</button>
+          </form>
+        </div>
       </div>
     )
   }
