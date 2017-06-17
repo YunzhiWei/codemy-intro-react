a5410fbed6ffdbc0741e71c5ecd82679e82171bf
Chris Wei <yunzhi.wei@live.com>
2017-06-17 21:26:34 +0800

    rename the folder from 'components' to 'pages' update routers.js accordingly

--- app/routes.js
+++ app/routes.js
@@ -2,19 +2,20 @@ import React from "react";
 
 import { Route, Redirect, IndexRoute } from 'react-router';
 
-import c from "./components";
+// import c from "./components";
+import p from "./pages";
 
 const routes =
   <Route>
     <Redirect from='/' to='/contacts' />
     <Route path='users'>
-      <Route path='sign_in' component={c.Sessions.New} />
-      <Route path='sign_up' component={c.Users.New} />
+      <Route path='sign_in' component={p.Sessions.New} />
+      <Route path='sign_up' component={p.Users.New} />
     </Route>
-    <Route component={c.Layout.Application}>
+    <Route component={p.Layout.Application}>
       <Route path='contacts'>
-        <IndexRoute component={c.Contacts.Collection} />
-        <Route path=':contactId' component={c.Contacts.Show} />
+        <IndexRoute component={p.Contacts.Collection} />
+        <Route path=':contactId' component={p.Contacts.Show} />
       </Route>
     </Route>
   </Route>;
