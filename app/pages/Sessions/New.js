6d6be298fecf19639f4e6ea6daef943867778c3c
Chris Wei <yunzhi.wei@live.com>
2017-06-17 21:58:53 +0800

    move New.sass into styles folder and rename it as button.sass update New.js accordingly

--- app/pages/Sessions/New.js
+++ app/pages/Sessions/New.js
@@ -5,7 +5,7 @@ import { Link } from "react-router";
 
 import page from 'components/page';
 
-import styles from './New.sass';
+import button from 'styles/button.sass';
 
 @inject('user') @observer
 class New extends React.Component {
@@ -31,7 +31,7 @@ class New extends React.Component {
         <form className='pure-form pure-form-stacked' onSubmit={this.onSubmitForm}>
           <label>Email</label><input type='email' ref={node => { this.email = node; }} placeholder='email' className='pure-input-1' />
           <label>Password</label><input type='password' ref={node => { this.password = node; }} placeholder='password' className='pure-input-1' />
-          <button className={`pure-button pure-input-1 ${styles.signInButton}`}>Sign In</button>
+          <button className={`pure-button pure-input-1 ${button.signInButton}`}>Sign In</button>
         </form>
       </page.Auth>
     )
